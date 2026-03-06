1f04950d122862e45261d713f8625ce6a00049e1
Ahnaf Abid <ahnafabid600@gmail.com>
2026-03-07 02:29:09 +0600

    promodoro updated

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -19,6 +19,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
         },
         { name: "Progress", href: "/dashboard/progress", icon: "trending_up" },
         { name: "Focus Timer", href: "/dashboard/focus-timer", icon: "timer" },
+        { name: "Calendar", href: "/dashboard/calender", icon: "calendar_today" },
     ];
 
     return (
